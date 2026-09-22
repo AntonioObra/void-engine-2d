2a325dca4bf555341ce4cbfe9ef16e19462eabd2
AntonioObra <antonio@obradovic.dev>
2026-09-22 19:09:50 +0200

    Added - ebiten, created a window

---
+++ main.go
@@ -0,0 +1,42 @@
+/**
+*
+*  /\_/\
+* ( o.o )
+*  > ^ <
+*
+* Main
+*
+* Main entry point for the Void Engine 2D
+*
+ */
+
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+type Game struct{}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello, World!")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
+	return 320, 240
+}
+
+func main() {
+	ebiten.SetWindowSize(640, 480)
+	ebiten.SetWindowTitle("Hello, World!")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
+}
